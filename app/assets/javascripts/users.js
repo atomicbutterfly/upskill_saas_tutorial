2b9f3bb6a25f33e2af18e775936864e651cd5c8e
Moodthy Alghorairi <moodthy@gmail.com>
2017-12-01 15:43:08 +0000

    added stripe responce handling js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,7 @@
 /* global $, Stripe */
 //document ready function
 $(document).on('turbolinks:load', function(){
-    var theForm =$('#pro_form');
+    var theForm = $('#pro_form');
     var submitBtn= $('#form-submit-btn');
     // set stripe public key
     Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content') );
@@ -9,28 +9,70 @@ $(document).on('turbolinks:load', function(){
     
     // when user clicks form submit button 
     submitBtn.click(function(event){
-        
+    
     // prevent default submission behaviour
         event.preventDefault();
+        submitBtn.val("Processing").prop('disabled', true);
         
     // collect credit card fields
         var ccNum = $('#card_number').val(),
             cvcNum =$('#card_code').val(),
             expMonth = $('#card_month').val(),
             expYear = $('#card_year').val();
-        //send card information to stripe
-         Stripe.createToken({
-            number : ccNum,
-            cvc: cvcNum,
-            exp_month: expMonth,
-            exp_year: expYear
-        }, stripeResponseHandler);
-    });
+            
+            
+        // use stripe JS library to check for card errors
+        var error = false;
         
+        // validate card number.
+        if(!Stripe.card.validateCardNumber(ccNum)){
+            error = true;
+            alert('the credit card number appears to be invalid');
+        }
+        
+    
+        
+         // validate CVC number
+        if (!Stripe.card.validateCVC(cvcNum)){
+            error = true;
+            alert('the cvc number appears to be invalid');
+        }
+        
+         // validate expiration date
+        if (!Stripe.card.validateExpiry(expMonth, expYear)){
+            error = true;
+            alert('the expiry date appear to be invalid');
+    
+        }
+        
+        if (error) {
+            //if there are card errors, dont send to stripe
+            submitBtn.prop('disabled', false).val("Sign up");
+            
+            } else {
+               
+                    // send card info to stripe 
+                    //send card information to stripe
+                 Stripe.createToken({
+                    number : ccNum,
+                    cvc: cvcNum,
+                    exp_month: expMonth,
+                    exp_year: expYear
+                }, stripeResponseHandler);
+                
+            }
+        return false;
+    });
+//stripe will return back a card token.
+function stripeResponseHandler(status, response) {
+    //get the token from the response
+   var token = response.id;
+   
+   // inject card token as hidden field
+   theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
 
-
-//stripe will return back a card token
-// injecr card token as hidden feild into form
 // submit form to our rails app
+    theForm.get(0).submit();
+}
 
 });
